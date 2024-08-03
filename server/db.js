6ae095a81b24b80c1bf31ab7818b12053d646425
ashanmillewa <ashanmillewa@gmail.com>
2024-08-03 23:56:11 +0530

    create client and server side
    create home page UI and setup the server side

---
+++ server/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+var mongoURL = 'mongodb+srv://ASHAN:11AIM2000@cluster0.knpswim.mongodb.net/hotel-rooms'
+
+mongoose.connect(mongoURL , {useUnifiedTopology : true, useNewUrlParser:true})
+
+var connection = mongoose.connection
+
+connection.on('error', ()=> {
+    console.log('Mongo DB Connection failed')
+})
+
+connection.on('connected', ()=> {
+    console.log('Mongo DB Connection Successful')
+})
+
+
+
+
+module.exports = mongoose
