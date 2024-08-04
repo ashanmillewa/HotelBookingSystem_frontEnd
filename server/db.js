b678d253b25e3bdebb0fcf8175622d7efa8609c0
ashanmillewa <ashanmillewa@gmail.com>
2024-08-04 23:41:51 +0530

    add room details

--- server/db.js
+++ server/db.js
@@ -1,20 +1,31 @@
+// const mongoose = require("mongoose");
+
+// var mongoURL = 'mongodb+srv://ASHAN:11AIM2000@cluster0.knpswim.mongodb.net/hotel-rooms'
+
+// mongoose.connect(mongoURL , {useUnifiedTopology : true, useNewUrlParser:true})
+
+// var connection = mongoose.connection
+
+// connection.on('error', ()=> {
+//     console.log('Mongo DB Connection failed')
+// })
+
+// connection.on('connected', ()=> {
+//     console.log('Mongo DB Connection Successful')
+// })
+
+
+
+
+// module.exports = mongoose
+
+
 const mongoose = require("mongoose");
 
-var mongoURL = 'mongodb+srv://ASHAN:11AIM2000@cluster0.knpswim.mongodb.net/hotel-rooms'
+var mongoURL = 'mongodb+srv://ASHAN:11AIM2000@cluster0.knpswim.mongodb.net/hotel-rooms';
 
-mongoose.connect(mongoURL , {useUnifiedTopology : true, useNewUrlParser:true})
+mongoose.connect(mongoURL)
+  .then(() => console.log('Mongo DB Connection Successful'))
+  .catch((err) => console.log('Mongo DB Connection failed', err));
 
-var connection = mongoose.connection
-
-connection.on('error', ()=> {
-    console.log('Mongo DB Connection failed')
-})
-
-connection.on('connected', ()=> {
-    console.log('Mongo DB Connection Successful')
-})
-
-
-
-
-module.exports = mongoose
+module.exports = mongoose;
